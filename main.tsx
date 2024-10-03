6e9a6d3a5925db89a0e78f323bdbbbe2d16dff36
simarjot <simarjot0032@gmail.com>
2024-10-04 01:30:53 +0530

    some file structure changes

---
+++ main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PunjabiVoiceSearch from "./PunjabiVoiceSearch";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <PunjabiVoiceSearch />
+  </React.StrictMode>
+);
